da83625954450909c5a5493c443b12b04a04bc24
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-11-01 20:20:59 +0100

    Initial commit

---
+++ ex001.py
@@ -0,0 +1,2 @@
+#program to read Hello world
+print('\033[0;31;44mhello world!\033[m')
---
+++ ex002.py
@@ -0,0 +1,4 @@
+#program to read a name and answer welcome message
+msg = input('type your name: ')
+#print('nice to meet you',msg)
+print('Nice to meet you {}'.format(msg))
