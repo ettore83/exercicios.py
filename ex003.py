d4dbc7d3b4f2241f7808d29256ae88d7643fa5e4
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-11-02 09:23:31 +0100

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,17 @@
+#create a program that reads two numbers and displays the sum between them
+n1 = int(input('type the first number: '))
+n2 = int(input('type the second number: '))
+soma = n1 + n2
+print('the sum between {} and {} it is {}'.format(n1, n2, soma))
+
+#create a program that reads two numbers and displays the division between them
+n3 = float(input('type the first float number: '))
+n4 = float(input('type the second float number: '))
+div = n3 / n4
+print('the division between {} and {} it is {}'.format(n3, n4, div))
+
+#create a program that reads two numbers and displays the concatenation between the words
+n5 = str(input('type your name: '))
+n6 = str(input('type your surname: '))
+conc = n5 + n6
+print('the concatenation between the words {} and {} will be like this {}'.format(n5, n6 ,conc ))
