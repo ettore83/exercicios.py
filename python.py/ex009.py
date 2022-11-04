7e4be03b135b9a64142fd31d5dd9403e47e7d269
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-11-04 09:32:26 +0100

    Create ex009.py

---
+++ python.py/ex009.py
@@ -0,0 +1,15 @@
+#read the integer number and show your multiplication table
+numb = int(input('type the number: '))
+#directly in print
+print('-'*12) #only to make a line
+print('{} x {:2} = {}'.format(numb , 1 , numb*1))
+print('{} x {:2} = {}'.format(numb , 2 , numb*2))
+print('{} x {:2} = {}'.format(numb , 3 , numb*3))
+print('{} x {:2} = {}'.format(numb , 4 , numb*4))
+print('{} x {:2} = {}'.format(numb , 5 , numb*5))
+print('{} x {:2} = {}'.format(numb , 6 , numb*6))
+print('{} x {:2} = {}'.format(numb , 7 , numb*7))
+print('{} x {:2} = {}'.format(numb , 8 , numb*8))
+print('{} x {:2} = {}'.format(numb , 9 , numb*9))
+print('{} x {:2} = {}'.format(numb , 10 , numb*10))
+print('-'*12)
