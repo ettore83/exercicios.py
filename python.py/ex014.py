48a6348d7745422268ec3ae4de42828588356749
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-11-05 09:30:41 +0100

    Create ex014.py

---
+++ python.py/ex014.py
@@ -0,0 +1,4 @@
+#program read the celsius temperature and convert in Fahrenheit 
+c = float(input('type the celsium temperature º '))
+f = ((9 * c) / 5) + 32
+print('the temperatur in celsius is {}ºC,and the conversion to Fahrenheit is {}ºF'.format(c , f))
