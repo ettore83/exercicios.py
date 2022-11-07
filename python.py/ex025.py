dfe032c0d7a3c8af51427cdf1c0c37bbe9f5e23a
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-11-07 10:44:37 +0100

    Create ex025.py

---
+++ python.py/ex025.py
@@ -0,0 +1,6 @@
+#looking for another string inside 
+#name = str(input('type your full name: ')).strip()
+#print('tem silva no nome:{}'.format('silva'in name.lower()))
+#em portugues
+nome = str(input('digite seu nome inteiro ')).strip()
+print('{} contem elias'.format('ELIAS'in nome.upper()))
