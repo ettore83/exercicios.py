8930984dfe03519363c08887fbaaa3db5422f986
Ettore Occhiena Elias <ettorepri@gmail.com>
2022-12-17 12:32:52 +0100

    Create ex097.py

---
+++ python.py/ex097.py
@@ -0,0 +1,14 @@
+#funcao de escrever 
+def escreva(msg):
+    tam = len(msg) + 4
+    print('^' * tam)
+    print(f'  {msg}')
+    print('^' * tam)
+    
+    
+    
+escreva('Ettore')
+escreva('Lorenzo')
+escreva('Priscila')
+escreva('Vicenzo')
+escreva('rua do vintem na rodovia 2345678')
